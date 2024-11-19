663a9f448405c6243b85acf93e1e37808f6fc68d
C0A23149 <c0a231491f@edu.teu.ac.jp>
2024-11-19 15:35:54 +0900

    kk読み込み、左右反転

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    pg3 = pg.image.load("fig/3.png")
+    pg3 = pg.transform.flip(pg3, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
