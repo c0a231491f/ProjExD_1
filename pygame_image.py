7cae1c10cce1fb608461af2f9f58195086725ec9
C0A23149 <c0a231491f@edu.teu.ac.jp>
2024-11-19 15:39:58 +0900

    kk貼り付け

--- pygame_image.py
+++ pygame_image.py
@@ -10,14 +10,15 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    pg3 = pg.image.load("fig/3.png")
-    pg3 = pg.transform.flip(pg3, True, False)
+    pg3 = pg.image.load("fig/3.png") #こうかとん読み込み
+    pg3 = pg.transform.flip(pg3, True, False) #こうかとん左右反転
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(pg3, [300, 200]) #こうかとん貼り付け
         pg.display.update()
         tmr += 1        
         clock.tick(10)
