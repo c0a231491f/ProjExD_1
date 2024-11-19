c2512272fb37cf639a5e97f5699253d53d328a72
C0A23149 <c0a231491f@edu.teu.ac.jp>
2024-11-19 16:21:45 +0900

    こうかとんのrect取得

--- pygame_image.py
+++ pygame_image.py
@@ -10,19 +10,23 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg_img_g = pg.transform.flip(bg_img, True, False)
-    pg3 = pg.image.load("fig/3.png") #こうかとん読み込み
-    pg3 = pg.transform.flip(pg3, True, False) #こうかとん左右反転
+    bg_img_g = pg.transform.flip(bg_img, True, False) #背景画像の左右反転
+    kpg3 = pg.image.load("fig/3.png") #こうかとん読み込み
+    kpg3 = pg.transform.flip(kpg3, True, False) #こうかとん左右反転
+    kpg3_rect = kpg3.get_rect() #こうかとんのrectの取得
+    kpg3_rect.center = 300, 200 #初期座標300，200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
         x = -(tmr%3200) #背景画像のX座標
-        screen.blit(bg_img, [x, 0]) #右から左に動く
+        screen.blit(bg_img, [x, 0]) #右から左に動く背景画像
         screen.blit(bg_img_g, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0]) 
         screen.blit(bg_img_g, [x+4800, 0])
-        screen.blit(pg3, [300, 200]) #こうかとん貼り付け
+
+        screen.blit(kpg3, kpg3_rect) #こうかとん貼り付け
         pg.display.update()
         tmr += 1        
         clock.tick(200) #FPS200
